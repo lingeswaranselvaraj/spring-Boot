18e524bd08e4882ae8e3e9b05467a8898d179333
linkesh <linkesheee@gmail.com>
2022-05-29 01:48:26 +0800

    postgres changes

--- src/main/java/com/springboot/java/student/student.java
+++ src/main/java/com/springboot/java/student/student.java
@@ -2,7 +2,27 @@ package com.springboot.java.student;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
 public class student {
+    @Id
+    @SequenceGenerator(
+        name = "student_sequence",
+        sequenceName = "student_sequence",
+        allocationSize = 1
+    )
+    @GeneratedValue(
+        strategy = GenerationType.SEQUENCE,
+        generator = "student_sequence"
+    )
+
     private Long Id;
     private String name;
     private Integer age;
